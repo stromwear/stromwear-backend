980369a159a04c1ae7349afd9515e5dbba1a4fba
= <=>
2025-07-18 07:46:48 +0530

    added cors url

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app:express.Application = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ["https://stromwear-frontend.vercel.app"],
+  origin: ['https://stromwear-frontend.vercel.app'],
   credentials: true,
 }));
 app.use("/api/users",UserRouter);
